9d3122a0e62eddecfe2b15b83bf5bf2935afbb9d
ashay <srivastavaashay2013@gmail.com>
2022-08-12 11:54:55 +0530

    Corrected Solutions of Borze and Beautiful Year

--- BeautifulYear.java
+++ BeautifulYear.java
@@ -1,2 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+
 public class BeautifulYear {
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out, true);
+        FastScanner sc = new FastScanner();
+        int year = sc.nextInt();
+        while(year<=9999){
+            year++;
+            HashSet<Character> set = new HashSet<>(Integer.toString(year).chars().mapToObj(i -> (char)i).collect(Collectors.toList()));
+            if(set.size()==4){
+                break;
+            }
+        }
+        out.println(year);
+    }
 }
--- Borze.java
+++ Borze.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Borze {
+// https://codeforces.com/problemset/problem/32/B
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Borze {
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out,true);
+        FastScanner sc = new FastScanner();
+        String input = sc.next();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i)=='-'){
+                if(input.charAt(i+1)=='.'){
+                    ans.append('1');
+                } else {
+                    ans.append('2');
+                }
+                i++;
+            } else {
+                ans.append('0');
+            }
+        }
+        out.println(ans);
+    }
 }
